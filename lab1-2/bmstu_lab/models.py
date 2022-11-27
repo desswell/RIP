79041f7b06e312d5eca757f8a906bb0b9d2ddc37
desswell <kam@hitekki.ru>
2022-11-27 18:04:14 +0300

    g

--- lab1-2/bmstu_lab/models.py
+++ lab1-2/bmstu_lab/models.py
@@ -1,28 +1,36 @@
-# from django.db import models
-#
-#
-# class Anime_title(models.Model):
-#     name = models.CharField(max_length=30)
-#     description = models.CharField(max_length=255)
-#
-#     class Meta:
-#         managed = True
-#         db_table = 'animes'
-#
-# class Studios(models.Model):
-#     name = models.CharField(max_length=50)
-#     description = models.CharField(max_length=255)
-#     rating = models.CharField(max_length=5)
-#
-#     class Meta:
-#         managed = True
-#         db_table = "studios"
-#
-# class Auth(models.Model):
-#     name = models.CharField(max_length=20)
-#     rating = models.CharField(max_length=10)
-#     country = models.CharField(max_length=30)
-#
-#     class Meta:
-#         managed = True
-#         db_table = "auth"
+from django.db import models
+
+
+class Anime_title(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+
+    class Meta:
+        managed = True
+        db_table = 'animes'
+
+class Studios(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    rating = models.CharField(max_length=5)
+
+    class Meta:
+        managed = True
+        db_table = "studios"
+
+class Auth(models.Model):
+    name = models.CharField(max_length=20)
+    rating = models.CharField(max_length=10)
+    country = models.CharField(max_length=30)
+
+    class Meta:
+        managed = True
+        db_table = "auth"
+
+class Comments(models.Model):
+    id = models.IntegerField(primary_key=True)
+    User_id = models.IntegerField()
+    text = models.CharField(max_length=30)
+    class Meta:
+        managed = True
+        db_table = 'comments'
--- lab1-2/bmstu_lab/views.py
+++ lab1-2/bmstu_lab/views.py
@@ -1,38 +1,61 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render
 
-data = [
-            {'title': 'Hunter x Hunter','descr': 'Ремейк аниме-сериала «Охотник х Охотник» 1999 года.','date': '2011' ,'id': 1},
-            {'title': 'Chainsaw Man','descr': 'У Дэндзи есть мечта — жить мирной и счастливой жизнью, проводя время с любимой девушкой.','date': '2022', 'id': 2},
-            {'title': 'Bleach: Sennen Kessen-hen','descr': 'Сообщество душ получает множество сообщений о тревоге:','date': '2021' , 'id': 3},
-    ]
-
-def GetAnimes(request):
-    return render(request, 'animes.html', {'data': {
-        'titles': data
-    }})
-
-
-def GetAnime(request, id):
-    descr = 'desc'
-    for i in data:
-        if i.get('id') == id:
-            descr = i.get('descr')
-
-    return render(request, 'anime.html', {'data': {
-        'id': id,
-        'url': 'images/'+str(id)+'.jpg',
-        'descr': descr,
-    }})
-
-# from bmstu_lab.models import Anime_title
+# data = [
+#             {'title': 'Hunter x Hunter','descr': 'Ремейк аниме-сериала «Охотник х Охотник» 1999 года.','date': '2011' ,'id': 1},
+#             {'title': 'Chainsaw Man','descr': 'У Дэндзи есть мечта — жить мирной и счастливой жизнью, проводя время с любимой девушкой.','date': '2022', 'id': 2},
+#             {'title': 'Bleach: Sennen Kessen-hen','descr': 'Сообщество душ получает множество сообщений о тревоге:','date': '2021' , 'id': 3},
+#     ]
 #
-# def animeList(request):
-#     return render(request, 'Animes1.html', {'data' : {
-#         'animes': Anime_title.objects.all()
+# def GetAnimes(request):
+#     return render(request, 'animes.html', {'data': {
+#         'titles': data
 #     }})
 #
-# def GetAnime1(request, id):
-#     return render(request, 'Anime1.html', {'data' : {
-#         'anime': Anime_title.objects.filter(id=id)[0]
+#
+#
+# def GetAnime(request, id):
+#     descr = 'desc'
+#     for i in data:
+#         if i.get('id') == id:
+#             descr = i.get('descr')
+#
+#     return render(request, 'anime.html', {'data': {
+#         'id': id,
+#         'url': 'images/'+str(id)+'.jpg',
+#         'descr': descr,
 #     }})
+
+from bmstu_lab.models import Anime_title, Comments
+
+
+def animeList(request):
+    return render(request, 'Animes1.html', {'data' : {
+        'animes': Anime_title.objects.all()
+    }})
+
+def GetAnime1(request, id):
+    return render(request, 'Anime1.html', {'data' : {
+        'anime': Anime_title.objects.filter(id=id)[0],
+        'comments': Comments.objects.filter(id=id)[0]
+    }})
+def sendText(request, id):
+    try:
+        import MySQLdb
+        db = MySQLdb.connect(
+            host="localhost",
+            user="dbuser",
+            passwd="123",
+            db="first_db"
+        )
+        c = db.cursor()
+        delete = f"DELETE FROM animes where id={id}"
+        c.execute(delete)
+        db.commit()
+        c.close()
+        db.close()
+        return render(request, 'Animes1.html', {'data': {
+            'animes': Anime_title.objects.all()
+        }})
+    except Anime_title.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
