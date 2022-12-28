10fe3f76f3b5e46c9eb2dc2d259476b1c4bd488e
desswell <kam@hitekki.ru>
2022-12-28 09:29:12 +0300

    8 lab

--- lab4 react/src/index.tsx
+++ lab4 react/src/index.tsx
@@ -17,4 +17,11 @@ root.render(
   </React.StrictMode>
 );
 
-    
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
